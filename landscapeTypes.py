659e8a4f6bcd9f8deb6510ce63d3bb6918fd291e
rodtaylor30 <sven674@web.de>
2017-04-28 21:47:59 +0200

    Create landscapeTypes.py

---
+++ landscapeTypes.py
@@ -0,0 +1,15 @@
+class LandscapeTypes(object):
+  FOREST = 0
+  MOUNTAIN = 1
+  LAKE = 2
+  HILL = 3
+  FORTRESS = 4
+  CITY = 5
+  KEEP = 6
+  PALACE = 7
+  TOWER = 8
+  RUINS = 9
+  DESERT = 10
+  MEADOW = 11
+  SWAMP = 12
+  SANDS = 13
