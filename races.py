a8dbdf401aa05a673cdafd3bd6c4e3f67105e7a6
rodtaylor30 <sven674@web.de>
2017-05-07 22:44:59 +0200

    Create races.py

---
+++ races.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python.
+
+"""
+    This file defines the races
+
+"""
+
+__author__ = "Sven Eggert"
+__copyright__ = "Copyright 2017, Egertiko Designs"
+__credits__ = []
+__license__ = "GPL"
+__version__ = "0.0.9"
+__maintainer__ = "Sven Eggert"
+__email__ = "sven674@web.de"
+__status__ = "Development"
+
+"""
+
+    The Landscape Types class
+
+"""
+class Race(object):
+  FIRST_RACE = 0
+  
+  DWARF = 0
+  GIANT = 1
+  HUMAN = 2
+  ELF = 3
+  DEADLORD = 4
+  
+  LAST_RACE = 4  
